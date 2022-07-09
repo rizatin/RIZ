4c51a00e4fecf4d90b8b39aff4457dc8bac615a4
rizatin <108764699+rizatin@users.noreply.github.com>
2022-07-09 15:52:58 +0100

    Create LOADER.lua

---
+++ LOADER.lua
@@ -0,0 +1,2 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/rizatin/RIZ/main/SMOOTH%20LOCK.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/rizatin/RIZ/main/MAIN.lua"))()
